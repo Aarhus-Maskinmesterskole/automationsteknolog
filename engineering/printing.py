d0f54fa3385b7cb372c685eaede80ccc7cb70a4a
Anders Østergaard <aso@aams.dk>
2026-02-09 11:21:49 +0100

    pisse fed python fil

--- engineering/printing.py
+++ engineering/printing.py
@@ -1,1 +1,7 @@
-print("min alder 38")
+name = input("indsæt pumpe navn: ")
+motorDriftTimer = int(input("indsæt motor drift timer: "))
+motorDriftMinutter = int(input("indsæt motor drift minutter: "))
+motorDriftSekunder = int(input("indsæt motor drift sekunder: "))
+motorIsOn = input("indsæt motor status (ON/OFF): ")
+motorStatus = input("Er motor OK? i Fejl? Vedligeholdelse?")
+print(f"Pumpe navn: {name}, Motor drift timer: {motorDriftTimer}, Motor drift minutter: {motorDriftMinutter}, Motor drift sekunder: {motorDriftSekunder}, Motor status: {motorStatus}, Motor er ON: {motorIsOn}")
